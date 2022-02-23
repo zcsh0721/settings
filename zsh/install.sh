15f399a7f01ad923525fdd510a5135183c793ee7
carey.zhou <carey.zhou@hgj.com>
2022-02-23 18:10:55 +0800

    修改 zsh 安装脚本

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,5 @@
+#  sh -c "$(curl -fsSL https://raw.githubusercontent.com/zcsh0721/settings/main/zsh/install.sh)"
+
 # 安装 zsh
 yum install -y zsh
 chsh -s /bin/zsh
@@ -10,8 +12,15 @@ yum install -y git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 
+# 下载 zshrc 配置
+wget https://raw.githubusercontent.com/zcsh0721/settings/main/zsh/zshrc -O ~/.zshrc
+
 # zsh 自动补全
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # zsh-syntax-highlighting 语法高亮
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+
+source ~/.zshrc
