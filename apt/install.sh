9a3cc52cdfa3f5cabcf8e8db809f6ec6628b256d
carey.zhou <carey.zhou@hgj.com>
2022-03-01 09:33:44 +0800

    apt

---
+++ apt/install.sh
@@ -0,0 +1,2 @@
+cp /etc/apt/sources.list /etc/apt/sources.bak1
+ln -s "${ZCSH_SETTING_DIR}/apt/sources.list" /etc/apt/sources.list
--- base_install.sh
+++ base_install.sh
@@ -1,3 +1,3 @@
 yum -y install gcc automake autoconf libtool make
-yum install -y gcc gcc-c++
+yum install -y gcc gcc-c++ cmake
 yum install -y git tree 
